99af106ccf2955147b78f555548412adfed236a2
ColexDev <cole@colex.dev>
2021-11-15 20:51:59 -0500

    Inital Commit

---
+++ src/def.h
@@ -0,0 +1,12 @@
+constexpr int KEY_SHIFT_H = 72;
+constexpr int KEY_SHIFT_J = 74;
+constexpr int KEY_SHIFT_K = 75;
+constexpr int KEY_SHIFT_L = 76;
+constexpr int KEY_H = 104;
+constexpr int KEY_J = 106;
+constexpr int KEY_K = 107;
+constexpr int KEY_L = 108;
+constexpr int KEY_Q = 113;
+constexpr int KEY_E = 101;
+constexpr int KEY_S = 115;
+
